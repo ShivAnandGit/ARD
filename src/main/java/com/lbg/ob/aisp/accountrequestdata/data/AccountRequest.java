3d502c1023522a7124901ed0cb93bd7c07a8aa1f
sthak4 <sthakur5.sapient.com>
2018-06-28 11:33:17 +0100

    Sonar minor fix

--- src/main/java/com/lbg/ob/aisp/accountrequestdata/data/AccountRequest.java
+++ src/main/java/com/lbg/ob/aisp/accountrequestdata/data/AccountRequest.java
@@ -26,179 +26,177 @@ import java.util.List;
 @Table(name = "ACCT_REQUEST")
 public final class AccountRequest {
 
-    @Id
-    @SequenceGenerator(name = "acct_request_id", sequenceName = "ACCT_REQUEST_SEQ", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "acct_request_id")
-    @Column(name = "ACCT_REQUEST_ID", updatable = false, nullable = false)
-    private Long accountRequestIdentifier;
+	@Id
+	@SequenceGenerator(name = "acct_request_id", sequenceName = "ACCT_REQUEST_SEQ", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "acct_request_id")
+	@Column(name = "ACCT_REQUEST_ID", updatable = false, nullable = false)
+	private Long accountRequestIdentifier;
 
-    @Column(name = "ACCOUNT_REQUEST_EXTERNAL_ID", updatable = false, nullable = false)
-    private String accountRequestExternalIdentifier;
+	@Column(name = "ACCOUNT_REQUEST_EXTERNAL_ID", updatable = false, nullable = false)
+	private String accountRequestExternalIdentifier;
 
-    @Column(name = "PROVIDER_CLIENT_ID", updatable = false, nullable = false)
-    private String providerClientId;
+	@Column(name = "PROVIDER_CLIENT_ID", updatable = false, nullable = false)
+	private String providerClientId;
 
-    @Column(name = "CREATED_DATE_TIME", updatable = false, nullable = false)
-    private Timestamp createdDateTime;
+	@Column(name = "CREATED_DATE_TIME", updatable = false, nullable = false)
+	private Timestamp createdDateTime;
 
-    @Column(name = "ACCOUNT_REQUEST_STATUS", updatable = true, nullable = false)
-    private String accountRequestStatus;
+	@Column(name = "ACCOUNT_REQUEST_STATUS", updatable = true, nullable = false)
+	private String accountRequestStatus;
 
-    @Column(name = "ACCOUNT_REQUEST_JSON_STRING", nullable = false, updatable = true, columnDefinition = "CLOB NOT NULL")
-    @Lob
-    private String accountRequestJsonString;
+	@Column(name = "ACCOUNT_REQUEST_JSON_STRING", nullable = false, updatable = true, columnDefinition = "CLOB NOT NULL")
+	@Lob
+	private String accountRequestJsonString;
 
-    @Column(name = "FAPI_FINANCIAL_ID", nullable = false, updatable = true)
-    private String fapiFinancialId;
+	@Column(name = "FAPI_FINANCIAL_ID", nullable = false, updatable = true)
+	private String fapiFinancialId;
 
-    @Column(name = "ENTITLEMENT_ID")
+	@Column(name = "ENTITLEMENT_ID")
 
-    private Long entitlementId;
-    
-    @Column(name = "ENT_ACCESS_CODE")
-    private String entitlementAccessCode;
+	private Long entitlementId;
 
-    @Transient
-    private List<String> permissions;
+	@Column(name = "ENT_ACCESS_CODE")
+	private String entitlementAccessCode;
 
+	@Transient
+	private List<String> permissions;
 
-    @Transient
-    private String permissionsExpirationDateTime;
+	@Transient
+	private String permissionsExpirationDateTime;
 
-    @Transient
-    private String transactionFromDateTime;
+	@Transient
+	private String transactionFromDateTime;
 
-    @Transient
-    private String transactionToDateTime;
+	@Transient
+	private String transactionToDateTime;
 
-    public AccountRequest() {
-        //DEFAULT CONSTRUCTOR
-    }
+	public AccountRequest() {
+		// DEFAULT CONSTRUCTOR
+	}
 
-    public AccountRequest(CreateAccountInputData createAccountInputData, String clientId, String fapiFinancialId, String json) throws UnsupportedEncodingException {
-        this.setAccountRequestExternalIdentifier(Util.createUniqueAccountRequestId());
-        this.setDefaultAccountRequestStatus();
-        this.setPermissions(createAccountInputData.getPermissions());
-        this.setCreatedDateTime();
-        this.setProviderClientId(clientId);
-        this.setAccountRequestJsonString(json);
-        this.setFapiFinancialId(fapiFinancialId);
-    }
+	public AccountRequest(CreateAccountInputData createAccountInputData, String clientId, String fapiFinancialId,
+			String json) throws UnsupportedEncodingException {
+		this.setAccountRequestExternalIdentifier(Util.createUniqueAccountRequestId());
+		this.setDefaultAccountRequestStatus();
+		this.setPermissions(createAccountInputData.getPermissions());
+		this.setCreatedDateTime();
+		this.setProviderClientId(clientId);
+		this.setAccountRequestJsonString(json);
+		this.setFapiFinancialId(fapiFinancialId);
+	}
 
+	private void setDefaultAccountRequestStatus() {
+		this.accountRequestStatus = AccountRequestStatusEnum.AWAITINGAUTHORISATION.getValue();
 
-    private void setDefaultAccountRequestStatus() {
-        this.accountRequestStatus = AccountRequestStatusEnum.AWAITINGAUTHORISATION.getValue();
+	}
 
-    }
+	@JsonProperty("AccountRequestIdentifier")
+	public Long getAccountRequestIdentifier() {
+		return accountRequestIdentifier;
+	}
 
-    @JsonProperty("AccountRequestIdentifier")
-    public Long getAccountRequestIdentifier() {
-        return accountRequestIdentifier;
-    }
+	public void setAccountRequestIdentifier(Long accountRequestIdentifier) {
+		this.accountRequestIdentifier = accountRequestIdentifier;
+	}
 
-    public void setAccountRequestIdentifier(Long accountRequestIdentifier) {
-        this.accountRequestIdentifier = accountRequestIdentifier;
-    }
+	@JsonProperty("AccountRequestId")
+	public String getAccountRequestExternalIdentifier() {
+		return accountRequestExternalIdentifier;
+	}
 
-    @JsonProperty("AccountRequestId")
-    public String getAccountRequestExternalIdentifier() {
-        return accountRequestExternalIdentifier;
-    }
+	public void setAccountRequestExternalIdentifier(String accountRequestExternalIdentifier) {
+		this.accountRequestExternalIdentifier = accountRequestExternalIdentifier;
+	}
 
-    public void setAccountRequestExternalIdentifier(String accountRequestExternalIdentifier) {
-        this.accountRequestExternalIdentifier = accountRequestExternalIdentifier;
-    }
+	@JsonProperty("ProviderClientId")
+	public String getProviderClientId() {
+		return providerClientId;
+	}
 
-    @JsonProperty("ProviderClientId")
-    public String getProviderClientId() {
-        return providerClientId;
-    }
+	public void setProviderClientId(String providerClientId) {
+		this.providerClientId = providerClientId;
+	}
 
-    public void setProviderClientId(String providerClientId) {
-        this.providerClientId = providerClientId;
-    }
+	public Timestamp getCreatedDateTime() {
+		return (Timestamp) createdDateTime.clone();
+	}
 
-    public Timestamp getCreatedDateTime() {
-        return (Timestamp) createdDateTime.clone();
-    }
+	private void setCreatedDateTime() {
+		this.createdDateTime = Util.getCurrentTimestamp();
+	}
 
-    private void setCreatedDateTime() {
-        this.createdDateTime = Util.getCurrentTimestamp();
-    }
+	@JsonProperty("Status")
+	public String getAccountRequestStatus() {
+		return accountRequestStatus;
+	}
 
-    @JsonProperty("Status")
-    public String getAccountRequestStatus() {
-        return accountRequestStatus;
-    }
+	public void setAccountRequestStatus(AccountRequestStatusEnum status) {
+		this.accountRequestStatus = status.getValue();
+	}
 
-    public void setAccountRequestStatus(AccountRequestStatusEnum status) {
-        this.accountRequestStatus = status.getValue();
-    }
+	@JsonProperty("Permissions")
+	public List<String> getPermissions() {
+		return permissions;
+	}
 
-    @JsonProperty("Permissions")
-    public List<String> getPermissions() {
-        return permissions;
-    }
+	public void setPermissions(List<String> permissions) {
+		this.permissions = permissions;
+	}
 
-    public void setPermissions(List<String> permissions) {
-        this.permissions = permissions;
-    }
+	@JsonProperty("ExpirationDateTime")
+	public String getPermissionsExpirationDateTime() {
+		return permissionsExpirationDateTime;
+	}
 
-    @JsonProperty("ExpirationDateTime")
-    public String getPermissionsExpirationDateTime() {
-        return permissionsExpirationDateTime;
-    }
+	public void setPermissionsExpirationDateTime(String permissionsExpirationDateTime) {
+		this.permissionsExpirationDateTime = permissionsExpirationDateTime;
+	}
 
-    public void setPermissionsExpirationDateTime(String permissionsExpirationDateTime) {
-        this.permissionsExpirationDateTime = permissionsExpirationDateTime;
-    }
+	@JsonProperty("TransactionFromDateTime")
+	public String getTransactionFromDateTime() {
+		return transactionFromDateTime;
+	}
 
-    @JsonProperty("TransactionFromDateTime")
-    public String getTransactionFromDateTime() {
-        return transactionFromDateTime;
-    }
+	public void setTransactionFromDateTime(String transactionFromDateTime) {
+		this.transactionFromDateTime = transactionFromDateTime;
+	}
 
-    public void setTransactionFromDateTime(String transactionFromDateTime) {
-        this.transactionFromDateTime = transactionFromDateTime;
-    }
+	@JsonProperty("TransactionToDateTime")
+	public String getTransactionToDateTime() {
+		return transactionToDateTime;
+	}
 
-    @JsonProperty("TransactionToDateTime")
-    public String getTransactionToDateTime() {
-        return transactionToDateTime;
-    }
+	public void setTransactionToDateTime(String transactionToDateTime) {
+		this.transactionToDateTime = transactionToDateTime;
+	}
 
-    public void setTransactionToDateTime(String transactionToDateTime) {
-        this.transactionToDateTime = transactionToDateTime;
-    }
+	@JsonIgnore
+	public String getAccountRequestJsonString() {
+		return accountRequestJsonString;
+	}
 
+	public void setAccountRequestJsonString(String json) {
+		this.accountRequestJsonString = json;
+	}
 
-    @JsonIgnore
-    public String getAccountRequestJsonString() {
-        return accountRequestJsonString;
-    }
+	public String getFapiFinancialId() {
+		return fapiFinancialId;
+	}
 
-    public void setAccountRequestJsonString(String json) {
-        this.accountRequestJsonString = json;
-    }
+	public void setFapiFinancialId(String fapiFinancialId) {
+		this.fapiFinancialId = fapiFinancialId;
+	}
 
-    public String getFapiFinancialId() {
-        return fapiFinancialId;
-    }
+	public Long getEntitlementId() {
+		return entitlementId;
+	}
 
-    public void setFapiFinancialId(String fapiFinancialId) {
-        this.fapiFinancialId = fapiFinancialId;
-    }
+	public void setEntitlementId(Long entitlementId) {
+		this.entitlementId = entitlementId;
+	}
 
-    public Long getEntitlementId() {
-        return entitlementId;
-    }
-
-    public void setEntitlementId(Long entitlementId) {
-        this.entitlementId = entitlementId;
-    }
-
-    public String getEntitlementAccessCode() {
-        return entitlementAccessCode;
+	public String getEntitlementAccessCode() {
+		return entitlementAccessCode;
 	}
 
 	public void setEntitlementAccessCode(String entitlementAccessCode) {
